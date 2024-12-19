064247c3b022469a1710ba4a4c1424168663c8ce
Elvis S. <elvissabanovic3@gmail.com>
2024-12-19 16:50:38 +0400

    feat: slow mat mul, running 6 hours

--- classes/matrix_mul_1/src/main.rs
+++ classes/matrix_mul_1/src/main.rs
@@ -1,3 +1,29 @@
+use rand::random;
+use std::time::Instant;
+
 fn main() {
-    println!("Hello from matrix mul 1");
+    const N: usize = 4096;
+
+    let mut a = vec![vec![0.0; N]; N];
+    let mut b = vec![vec![0.0; N]; N];
+    let mut c = vec![vec![0.0; N]; N];
+
+    for i in 0..N {
+        for j in 0..N {
+            a[i][j] = random::<f64>();
+            b[i][j] = random::<f64>();
+        }
+    }
+
+    let start = Instant::now();
+    for i in 0..N {
+        for j in 0..N {
+            for k in 0..N {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    let duration = start.elapsed();
+    println!("{:.6}", duration.as_secs_f64());
 }
