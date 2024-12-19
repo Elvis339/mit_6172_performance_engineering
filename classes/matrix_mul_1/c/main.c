e11b9e892db78ce578cb289c65dd5de972167cfd
Elvis S. <elvissabanovic3@gmail.com>
2024-12-19 20:51:29 +0400

    feat(c/mat_mul): Set exec speed to 1.825093 by parallelized loops and improved cache locality

---
+++ classes/matrix_mul_1/c/main.c
@@ -0,0 +1,58 @@
+//
+// Created by Elvis Sabanovic on 19/12/2024.
+//
+#include <stdlib.h>
+#include <stdio.h>
+#include "sys/time.h"
+#include <cilk/cilk.h>
+
+#define N 4096
+
+double A[N][N];
+double B[N][N];
+double C[N][N];
+
+float time_diff(struct timeval *start, struct timeval *end) {
+  return (end->tv_sec - start->tv_sec) + 1e-6 * (end->tv_usec - start->tv_usec);
+}
+
+int main(int argc, const char *argv[]) {
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      A[i][j] = (double)rand() / (double)RAND_MAX;
+      B[i][j] = (double)rand() / (double)RAND_MAX;
+      C[i][j] = 0.0;
+    }
+  }
+
+  struct timeval start, end;
+  gettimeofday(&start, NULL);
+
+  int s = 64;
+  int t = 64;
+
+  cilk_for (int ih = 0; ih < N; ih += s) {
+    cilk_for (int jh = 0; jh < N; jh += s) {
+      for (int kh = 0; kh < N; kh += s) {
+        for (int im = 0; im < s; im += t) {
+          for (int jm = 0; jm < s; jm += t) {
+            for (int km = 0; km < s; km += t) {
+              for (int il = 0; il < t; ++il) {
+                for (int kl = 0; kl < t; ++kl) {
+                  for (int jl = 0; jl < t; ++jl) {
+                    C[ih+im+il][jh+jm+jl] +=
+                      A[ih+im+il][kh+km+kl] * B[kh+km+kl][jh+jm+jl];
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  gettimeofday(&end, NULL);
+  printf("%0.6f\n", time_diff(&start, &end));
+  return 0;
+}
