01d487f740f345ff8c133fcd80aa32dcb71d1f55
Elvis S. <elvissabanovic3@gmail.com>
2025-01-07 11:27:46 +0400

    feat: wrapped malloc

--- homework/6_mem_allocator/malloc.c
+++ homework/6_mem_allocator/malloc.c
@@ -27,14 +27,14 @@ void* simple_malloc(size_t size) {
     return NULL;
   }
 
-  // adjust size for alignment
-  // this is known bit trick for  rounding down to the nearest multiple of 8
-  //  7 = 0 0 0 0 0 1 1 1
-  // ~7 = 1 1 1 1 1 0 0 0
+  // adjust size to ensure 8-byte alignment by rounding up to the nearest multiple of 8
+  // This bit manipulation technique works by:
+  // 1. Adding 7 to size to prepare for rounding
+  // 2. Using bitwise AND with inverted 7 to clear the last 3 bits
   // Example: size = 21
-  // 21 = 0 0 0 1 0 1 0 1
-  // ~7 = 1 1 1 1 1 0 0 0
-  // 21 & ~7 = 0 0 0 1 0 0 0 0 = 16
+  // 21 + 7 = 28    (0001 1100)
+  // ~7     =       (1111 1000)
+  // Result  =      (0001 1000) = 24
   size_t aligned_size = (size + 7) & ~7;
 
   // current break point
@@ -47,4 +47,43 @@ void* simple_malloc(size_t size) {
 
   // return the old break point
   return current;
+}
+
+// must be 8-byte aligned itself
+typedef struct header {
+  size_t size;        // total size including header
+  uint32_t magic;     // magic number to detect corruption
+} header_t;
+
+// used to detect memory corruption and helps identify
+// if someone wrote before the start of their allocated block
+#define MAGIC_NUMBER 0xDEADBEEF
+
+/**
+* Low Address  [Header][....User Data....]  High Address
+            ↑       ↑
+            |       |
+            |       +-- Pointer returned to user
+            +-- Actual start of allocated block
+*/
+void* wrapped_malloc(size_t size) {
+  if (size == 0) {
+    return NULL;
+  }
+
+  size_t total_size = size + sizeof(header_t);
+  size_t aligned_size = (total_size + 7) & ~7;
+
+  void* block = sbrk(aligned_size);
+  if (block == (void*)-1) {
+    return NULL;  // OOM
+  }
+
+  // init header
+  header_t* header = (header_t*)block;
+  header->size = aligned_size;
+  header->magic = MAGIC_NUMBER;
+
+  // pointer to memory (just past header)
+  return (void*)(header + 1);
 }
--- homework/6_mem_allocator/malloc.h
+++ homework/6_mem_allocator/malloc.h
@@ -4,10 +4,13 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stddef.h>
 
 #ifndef MALLOC_H
 #define MALLOC_H
 
 void *simple_malloc(size_t size);
+
+void *wrapped_malloc(size_t size);
 
 #endif //MALLOC_H
